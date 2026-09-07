3cc6dee2b8ad6ac06edd4d6e5297eacafb6fe6d3
SAeed.ajf <ajfandaks@gmail.com>
2026-09-07 03:39:30 +0330

    Align NanoPino minimum runtime with tested Pincore 3.14 floor

--- payload/Cms/Support/CmsRelease.php
+++ payload/Cms/Support/CmsRelease.php
@@ -14,7 +14,7 @@ namespace App\com_pinoox_cms\Cms\Support;
  */
 final class CmsRelease
 {
-    private const DEFAULT_MIN_PINCORE = '3.10.0';
+    private const DEFAULT_MIN_PINCORE = '3.14.0';
 
     /** @var array<string,mixed>|null */
     private static ?array $metadata = null;

0d8d2bfb048850de17a062260d8a6fb681314560
SAeed.ajf <ajfandaks@gmail.com>
2026-09-07 17:48:59 +0330

    feat(sdk): expose Developer generator contract in admin boot

--- payload/Controller/AdminController.php
+++ payload/Controller/AdminController.php
@@ -24,6 +24,7 @@ use Pinoox\Component\Http\Request;
 use Pinoox\Component\Helpers\PinooxScriptHelper;
 use Pinoox\Portal\View;
 use App\com_pinoox_cms\Cms\Api\V1\Builder\BuilderApiContract;
+use App\com_pinoox_cms\Cms\Api\V1\Developer\DeveloperApiContract;
 use App\com_pinoox_cms\Cms\Api\V1\Extension\ExtensionApiContract;
 use App\com_pinoox_cms\Cms\Api\V1\Update\UpdateApiContract;
 use App\com_pinoox_cms\Cms\Api\V1\Recovery\RecoveryApiContract;
@@ -611,6 +612,8 @@ final class AdminController extends Controller
                                 'onRoute','onApi','onPath','onAction','onController','onModel','onTheme',
                             ],
                             'apiBase' => '/api/v1/extensions/{package}',
+                            'generatorApi' => DeveloperApiContract::routes(),
+                            'generatorApiBound' => RuntimeBindingState::api(),
                             'starters' => [],
                             'packageValidator' => true,
                             'testHarness' => true,
